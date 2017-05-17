07dfabc6ad1e36f2f37b36e92cbb6a696da79d17
Karthik Iyengar <karthikeyan.iyengar@gmail.com>
2017-05-18 00:51:35 +0530

    Check autotrack

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,31 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import styleSheet from 'styled-components/lib/models/StyleSheet';
-import ReactGA from 'react-ga';
+
+const GOOGLE_ANALYTICS_UA = 'UA-99297912-1';
+if (typeof window !== 'undefined') {
+  // eslint-disable-next-line
+  // window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  // /* global ga: true */
+  ga('create', GOOGLE_ANALYTICS_UA, 'auto');
+
+  // // autotrack
+  // // https://github.com/googleanalytics/autotrack
+
+  ga('require', 'urlChangeTracker');
+  ga('require', 'cleanUrlTracker');
+  ga('require', 'outboundFormTracker');
+  ga('require', 'outboundLinkTracker');
+
+  // // check out more here https://github.com/googleanalytics/autotrack#plugins
+
+  // // now that everything is ready, log initial page
+  ga('set', 'page', window.location.pathname);
+  ga('send', 'pageview');
+  console.log('pageview');
+}
+
 
 export default class MyDocument extends Document {
-  componentDidMount() {
-    ReactGA.initialize('UA-99297912-1');
-  }
-
   static async getInitialProps({ renderPage }) {
     const page = renderPage();
     const styles = (
@@ -19,10 +38,12 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width"  />
           <title>Karthik Iyengar</title>
           <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway:200" rel="stylesheet" />
-          <script src='https://www.google.com/recaptcha/api.js'></script>
+          <script src="https://www.google.com/recaptcha/api.js" />
+          <script async src="https://www.google-analytics.com/analytics.js"></script>
+          <script async src="/static/js/autotrack.js"></script>
         </Head>
         <body>
           <Main />
