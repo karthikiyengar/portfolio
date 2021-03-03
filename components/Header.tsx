e99d2837393559c37b57c74d069d4b2db5edba37
Karthik Iyengar <hello@kiyengar.net>
2021-03-03 12:06:22 +0100

    Change header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
 
+export const LinksContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1fr;
+`;
+
 export const Header = styled.header`
   min-height: 100px;
   display: flex;
   justify-content: space-between;
-  align-items: flex-end;
+  align-items: center;
 `;
 
 export const Title = styled.h1`
@@ -18,10 +24,19 @@ export const Title = styled.h1`
   }
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  display: none;
+`;
+
 export default () => (
   <Header>
     <Link href="/">
       <Title>Karthik Iyengar</Title>
     </Link>
+    <LinksContainer>
+      <StyledLink href="/blog">Blog</StyledLink>
+      <StyledLink href="/">About</StyledLink>
+    </LinksContainer>
   </Header>
 );
