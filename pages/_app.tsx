5f269b2bdf754a737b1f2f8f51230df335b58b9a
Karthik Iyengar <hello@kiyengar.net>
2023-01-10 22:14:48 +0530

    Add canonical URLs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 import Head from "next/head";
-import App from "next/app";
+import { AppProps } from "next/app";
 import { Global } from "../components/styled";
 import { Normalize } from "styled-normalize";
+import { useRouter } from "next/router";
 import "prismjs/themes/prism-tomorrow.css";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <title>Karthik Iyengar</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Normalize />
-        <Global />
-        <Component {...pageProps} />
-      </>
-    );
-  }
+const MyApp: React.FC<AppProps> = (props) => {
+  const router = useRouter();
+  const canonicalUrl = (`https://kiyengar.net` + (router.asPath === "/" ? "" : router.asPath)).split("?")[0];
+
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>Karthik Iyengar</title>
+        <link rel="canonical" href={canonicalUrl} />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+      </Head>
+      <Normalize />
+      <Global />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
